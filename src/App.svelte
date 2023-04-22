4cee7d5af9aa0d0f2d671187cb50edaa7f1efe46
raven <zlat.dominic.francisc@gmail.com>
2023-04-22 15:53:00 +0300

    [Setting Up basic components]

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from "svelte";
 	import { Map } from "@onsvisual/svelte-maps";
 	import maplibre from "maplibre-gl";
+	import Button from "./components/Button.svelte";
 
 	let map;
 	let zoom = 0;
@@ -10,14 +11,18 @@
 	let lat = 49.2125578;
 
 	onMount(() => {
-		map.addControl(new maplibre.NavigationControl(), "top-left");
+		map.addMarker;
 	});
 
 	function pans() {
-		screen.orientation
+		window.screen.orientation
 			.lock("landscape")
-			.then((res) => {})
-			.catch((e) => {});
+			.then((res) => {
+				console.log("yeey");
+			})
+			.catch((e) => {
+				console.log("not yeeey");
+			});
 		document.documentElement.requestFullscreen();
 		setInterval(() => {
 			lng += 0.000005;
@@ -39,8 +44,37 @@
 	/>
 	<div on:click={pans} id="fullScreenButton">Go Full Screen</div>
 </main>
+<Button
+	top="50%"
+	color="#2400ff"
+	borderColor="#2400ff"
+	label={lat.toFixed(5)}
+	width="20%"
+	height="5%"
+	onClick={() => {
+		console.log("weee");
+	}}
+	backgroundColor="#2400ff20"
+/>
+<Button
+	top="55%"
+	color="#2400ff"
+	borderColor="#2400ff"
+	label={lng.toFixed(5)}
+	width="20%"
+	height="5%"
+	onClick={() => {
+		console.log("weee");
+	}}
+	backgroundColor="#2400ff20"
+/>
 
 <style lang="scss">
+	:global(body) {
+		background-color: #000;
+		font-family: "Electrolize", sans-serif;
+	}
+
 	main {
 		width: 100%;
 		height: 150vh;
---
+++ src/components/Button.svelte
@@ -0,0 +1,42 @@
+<script>
+    let label;
+    let className;
+    let color;
+    let borderColor;
+    let backgroundColor;
+    let onClick;
+    let width;
+    let height;
+    let top;
+    let left;
+    export {
+        onClick,
+        label,
+        className,
+        color,
+        borderColor,
+        backgroundColor,
+        width,
+        height,
+        top,
+        left,
+    };
+</script>
+
+<div
+    on:click={onClick}
+    class={`button ${className}`}
+    style="left: {left}; top: {top}; width: {width}; height: {height}; color: {color}; border: solid 1px {borderColor}; background-color: {backgroundColor}"
+>
+    {label}
+</div>
+
+<style>
+    .button {
+        position: absolute;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        top: 0%;
+    }
+</style>
